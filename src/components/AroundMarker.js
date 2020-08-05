3c2af338ab8bb22f5cacf51111ea5ab9b45b8887
siaohuozhi <lkl19970321@gmail.com>
2020-08-05 00:12:16 -0500

    init commit

---
+++ src/components/AroundMarker.js
@@ -0,0 +1,57 @@
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+import blueMarkerUrl from '../assets/images/blue-marker.svg'
+import { Marker, InfoWindow } from 'react-google-maps';
+
+class AroundMarker extends Component {
+    static propTypes = {
+        post: PropTypes.object.isRequired,
+    }
+
+    state = {
+        isOpen: false,
+    }
+
+    handleToggle = () => {
+        this.setState((prevState) => ({ isOpen: !prevState.isOpen }));
+    }
+
+    render() {
+        const { user, message, url, location, type } = this.props.post;
+        const { lat, lon } = location;
+
+        const isImagePost = type === 'image';
+        const customIcon = isImagePost ? undefined : {
+            url: blueMarkerUrl,
+            scaledSize: new window.google.maps.Size(26, 41),
+        };
+
+        return (
+            <Marker
+                position={{ lat, lng: lon }}
+                // onMouseOver={this.handleToggle}
+                // onMouseOut={this.handleToggle}
+                // onClick={this.handleToggle}
+                onMouseOver={isImagePost ? this.handleToggle : undefined}
+                onMouseOut={isImagePost ? this.handleToggle : undefined}
+                onClick={isImagePost ? undefined: this.handleToggle}
+                icon={customIcon}
+            >
+                    {this.state.isOpen ? (
+                            <InfoWindow>
+                                <div>
+                                    {/*<img src={url} alt={message} className="around-marker-image"/>*/}
+                                    {/*<p>{`${user}: ${message}`}</p>*/}
+                                    {isImagePost
+                                        ? <img src={url} alt={message} className="around-marker-image"/>
+                                        : <video src={url} controls className="around-marker-video"/>}
+                                    <p>{`${user}: ${message}`}</p>
+                                </div>
+                            </InfoWindow>
+                        ) : null}
+            </Marker>
+        );
+    }
+}
+
+export default AroundMarker;
